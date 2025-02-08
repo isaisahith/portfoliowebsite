d6d9948074686321c9df670e327606a34fd3a827
Sai Sahith <saisahithinakota@gmail.com>
2025-02-08 19:14:41 +0530

    image urls updated

--- src/home/Home.js
+++ src/home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
           <p className='aboutme'>I am a Full Stack Developer with 4 years of experience specializing in building scalable web applications. My expertise includes React, JavaScript, TypeScript, HTML, CSS on the frontend and Java, Spring Boot, Hibernate, MySQL on the backend. I enjoy creating efficient, user-friendly solutions and continuously improving my skills to stay ahead in the ever-evolving tech landscape. 🚀</p>
         </div>
         <div className="about-image">
-          <img src="https://drive.google.com/file/d/1RMs1gh6mE_TJ4hhMM2eVyu9rnKHT1eTk/view?usp=drive_link" alt="Curious designer" />
+          <img src="./hero.jpg" alt="Curious designer" />
         </div>
       </section>
     </div>
